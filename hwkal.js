ef7d9f0a6e10f629520b454551bdeb99b44da290
Putrahendika <putrashoop1@gmail.com>
2023-07-30 02:24:16 +0700

    stz

---
+++ hwkal.js
@@ -0,0 +1,50 @@
+// Buat Lu Yang Jual Sc Ini Yang Jujur Jangan Samp Nipu Apalagi Lari Dari Tanggung Jawab
+
+// Base Haikal Hw Mods : Copyan Dari Base Dika
+
+
+const fs = require('fs')
+const chalk = require('chalk')
+const tiktok = require("./baseikal/download/tiktok")
+const facebook = require("./baseikal/download/facebook")
+const instagram = require("./baseikal/download/instagram")
+const twitter = require("./baseikal/download/twitter")
+
+
+//MODULNYA BY HW
+global.baileys = require('@whiskeysockets/baileys') 
+global.adiwajshing = require('@adiwajshing/baileys') 
+
+global.gr = 'https://chat.whatsapp.com/DuCMeq3ZK6HLvTzTma9HQj'
+global.ig = '@aingmacan' // ubah aja
+global.email = 'helloh@gmail.com' //serah
+global.region = 'amerika' // serah
+//—————「 Set Nama Own & Bot 」—————//
+global.ownername = 'Fbi' //ubah jadi nama mu, note tanda ' gausah di hapus!
+//=================================================//
+global.owner = ['6283824035969'] // ubah aja pake nomor lu
+//==========================BY Hw Mods=======================//
+global.keyopenai = `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`
+//====================BY Hw Mods=============================//
+global.botname = 'Fbi Bot' //ubah jadi nama bot mu, note tanda ' gausah di hapus!
+global.packname = 'Fbi' // ubah aja ini nama sticker
+global.author = 'Fbi' // ubah aja ini nama sticker
+global.prefa = ['','!','.',',','🐤','🗿']
+global.sessionName = 'haikal' //Gausah Juga
+global.sp = '⭔' // Gausah Juga
+global.wlcm = []
+global.wlcmm = []
+//=================================================//
+//Gausah Juga
+global.limitawal = {
+    premium: "Infinity",
+    free: 10
+}
+//=================================================//
+let file = require.resolve(__filename)
+fs.watchFile(file, () => {
+	fs.unwatchFile(file)
+	console.log(chalk.redBright(`Update'${__filename}'`))
+	delete require.cache[file]
+	require(file)
+})
